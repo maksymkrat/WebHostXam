1a6275b49444a686ecd533204f6cfff401d48548
mkrat <mkrat@mint-innovations.com>
2023-02-28 16:26:12 +0200

    finish extra offers

---
+++ WebHostXam/Models/ExtraOffer.cs
@@ -0,0 +1,12 @@
+﻿namespace WebHostXam.Models
+{
+    public class ExtraOffer
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgBase64Str { get; set; }
+        public float OldPrice { get; set; }
+        public float NewPrice { get; set; }
+        
+    }
+}
